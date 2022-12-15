f30eefc94c2cb724eaf5cb99de7a50b134c93e3e
Crowdbotics <team@crowdbotics.com>
2022-12-15 11:06:50 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,10 +1,21 @@
+const duration = null
+const onDurationEnd = null
+const hide = () => SplashScreen.hide()
+const show = () => SplashScreen.show()
 export const globalOptions = {
   name: "tahweela_37777",
   url: "https://tahweela_37777.botics.co",
   api: "https://tahweela_37777.botics.co/api/v1"
-};
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  "@modules/splash": {
+    duration: duration,
+    onDurationEnd: onDurationEnd,
+    hide: hide,
+    show: show
   }
-};
+}
