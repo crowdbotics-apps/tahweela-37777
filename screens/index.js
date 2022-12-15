fd4d11356277553d7e412c1ee2f22491de831ae4
Crowdbotics <team@crowdbotics.com>
2022-12-15 11:39:24 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,84 +1,110 @@
 import React from "react";
-import { Text, StyleSheet, View, Image } from "react-native";
+import { Text, View, StyleSheet, Image, Pressable } from "react-native";
 
-const WalletSwitchValidation = (params) => {
+const CloseTransaction = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.head}>
-        <Image source={require("./assets/Icon.png")}/>
-        <Text style={styles.heading}>Validate transaction</Text>
-      </View>
-      <View style={styles.keyContainer}>
-        <View style={styles.key}>
-          <Text>Public key</Text>
-          <Text style={styles.addressTxt}>0x546545..00005</Text>
+      <View style={styles.header}>
+        <View style={styles.textContainer}>
+          <Text style={styles.heading}>Close Transaction?</Text>
+          <Text style={styles.subheading}>Are you sure you want to close?</Text>
         </View>
-        <View style={styles.separator}></View>
-        <View style={styles.key}>
-          <Text>Private key</Text>
-          <Text style={styles.addressTxt}>0x546545..00005</Text>
-        </View>
-
+        <Image source={require("./assets/warningIcon.png")} />
       </View>
-      <View style={styles.text}>
-        <Text style={styles.innerText}>
-        Transaction has been officially recorded and verified, the payment will be processed, and it can no longer be reversed.
+      <View style={styles.body}>
+        <Text style={styles.bodyHeading}>Attention!</Text>
+        <Text style={styles.bodyText}>
+          The process of moving funds will be stallet.
         </Text>
       </View>
+      <Button buttonText={"Continue the process"} />
+      <Button buttonText={"Cancel"} outline={true} />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  header: {
+    backgroundColor: "#F1F1F1",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20
+  },
+  heading: {
+    fontSize: 22,
+    marginBottom: 10
+  },
+  subheading: {
+    fontSize: 14,
+    color: "#8E8E93"
+  },
+  body: {
+    flex: 1,
+    padding: 20
+  },
+  bodyHeading: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#EA4335",
+    marginBottom: 10
+  },
+  bodyText: {
+    fontSize: 17,
+    color: "#8E8E93",
+    width: "70%"
+  }
+});
+
+export default CloseTransaction;
+const Button = params => {
+  const btnStyle = {
+    backgroundColor: params.outline ? "#fff" : "#000",
+    borderColor: params.outline ? "#000" : "#fff",
+    borderWidth: 1
+  };
+  const btnText = {
+    color: params.outline ? "#000" : "#fff"
+  };
+  return (
+    <View style={buttonStyles.btnContainer}>
+      <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
+        <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
+        <View style={styles.childrenContainer}>{params.children}</View>
+      </Pressable>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    flex: 1,
-    backgroundColor: "#fff"
+const buttonStyles = StyleSheet.create({
+  btnContainer: {
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    marginBottom: 20
   },
-  head: {
+  btn: {
+    backgroundColor: "black",
+    height: 50,
+    width: "100%",
+    padding: 10,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    elevation: 10,
+    flexDirection: "row"
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  childrenContainer: {
     justifyContent: "center",
     alignItems: "center"
-  },
-  keyContainer: {
-    padding: 10,
-    shadowColor: "rgba(0,0,0,0.2)",
-    elevation: 15,
-    marginBottom: 15,
-    borderRadius: 10
-  },
-  heading: {
-    fontSize: 22,
-    fontWeight: "bold",
-    marginVertical: 15
-  },
-  key: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    padding: 15
-  },
-  separator: {
-    height: 1,
-    backgroundColor: "#F0F2F7",
-    marginHorizontal: 10
-  },
-  text: {
-    padding: 10,
-    shadowColor: "rgba(0,0,0,0.2)",
-    elevation: 10,
-    borderRadius: 10
-  },
-  innerText: {
-    padding: 10,
-    borderRadius: 10
-  },
-  addressTxt: {
-    color: "#7C7C7C",
-    fontSize: 12
   }
-
 });
-
-export default WalletSwitchValidation;
